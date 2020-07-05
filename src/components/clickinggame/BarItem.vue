88e2bee008176d0855711f18de471f198ba09cfb
Johanna <johannagwan@gmail.com>
2020-07-05 09:58:13 +0800

    add skeleton code for clicking game

---
+++ src/components/clickinggame/BarItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bar-item">
+      <p>
+        {{bar.name}}
+        {{bar.percentage}}
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BarItem",
+  props: ["bar"],
+}
+</script>
+
+<style scoped>
+  .bar-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+</style>
