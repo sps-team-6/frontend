816d383878498f70b16cf40c1132003bdb421ea7
Kendrick Ang <kendrick.ang@ninjavan.co>
2020-07-06 22:04:45 +0800

    add skeleton for typing game

---
+++ src/components/typinggame/TypingGame.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div id="text-display">
+
+        </div>
+        <input id="input-field" type="text" />
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                wordList: ["Help", "me", "what", "am", "I", "doing"],
+                currentWord: 0,
+                correctWords: 0,
+                incorrectWords: 0,
+            }
+        },
+        mounted: function() {
+            this.showText()
+            this.registerHandler()
+        },
+        methods: {
+            showText: function() {
+                const textDisplay = document.getElementById('text-display')
+                this.wordList.forEach(word => {
+                    const span = document.createElement("span")
+                    span.innerText = word + " "
+                    textDisplay.appendChild(span)
+                })
+                textDisplay.firstChild.classList.add('highlighted')
+            },
+            registerHandler: function() {
+                const inputField = document.getElementById("input-field")
+                inputField.addEventListener("keydown", e => {
+                    // space represents submission of a word
+                    if (e.key === " ") {
+
+                        console.log("HELLO WORLD")
+                    }
+                })
+            },
+        },
+    }
+</script>
+
+<style>
+.highlighted {
+    color: lightslategray;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Typing from '../components/typinggame/TypingGame'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/typing',
+    name: 'Typing',
+    component: Typing
   },
   {
     path: '/about',
