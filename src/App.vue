31553728f24069799f4b20f1df6b65f7dc5a6700
SherylNg <iamsherylng@gmail.com>
2020-07-07 19:05:19 +0800

    Add homepage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link> 
     </div>
     <router-view/>
   </div>
@@ -14,19 +13,29 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  list-style-type: none;
+  margin: 0;
+  padding:0;
+  overflow: hidden;
+  background-color: rgb(68, 64, 64);
+
+}
+#nav a{
+  display: block;
+  color: white;
+  text-align: left;
+  padding: 14px 14px;
+  text-decoration: none;
+}
+v-btn{
+    color :grey;
+    border: grey;
+    width: 20px;
+    height: 20px;
+
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
+
+const opts = {}
+
+export default new Vuetify(opts)
 
 new Vue({
   router,
