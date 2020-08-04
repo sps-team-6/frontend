1a7f58da49d35b0f4bad532bfb1dbf814d4299e6
Kendrick Ang <kendrick.ang@ninjavan.co>
2020-08-05 00:09:10 +0800

    debug deployment

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -95,8 +95,7 @@
             // populates rooms object; returns { typing: [{roomNo, maxPlayersCount, currentPlayersCount]}, clicking: [...] }
             getRooms: async function() {
                 const newRooms = {}
-                console.log(process.env.VUE_APP_SERVER_URL)
-                await fetch(`${process.env.VUE_APP_SERVER_URL}/rooms`)
+                await fetch(`https://secure-sierra-78883.herokuapp.com/rooms`)
                     .then(res => res.json())
                     .then(rooms => {
                         rooms.games.forEach(o => newRooms[o.name] = [...o.rooms])
